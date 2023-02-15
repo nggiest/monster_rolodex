e550fd337a2b8974e8ff1cd5b4931479398d9fa8
agrgiova8892 <giovanni.anggiesta@ai.astra.co.id>
2023-02-15 20:27:25 +0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -8,24 +10,18 @@ class App extends Component {
       monsters : [],
       searchField: ''
     }
-    console.log('constructor');
   }
 
   componentDidMount(){
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => (response.json()))
       .then((users)=> this.setState(()=> {
         return {monsters :users }
-      },
-      () => {
-        console.log(this.state)
       }
       )) 
   }
 
   onSearchChange = (event) => {
-    console.log(event.target.value);
     const searchField = event.target.value.toLocaleLowerCase();
     
     this.setState(() => {
@@ -34,8 +30,6 @@ class App extends Component {
   } 
 
   render() {
-    console.log('render');
-    
     const {monsters, searchField} = this.state;
     const {onSearchChange} = this;
     
@@ -46,21 +40,8 @@ class App extends Component {
     
     return (
       <div className="App">
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search-monster' 
-          onChange={onSearchChange}
-        />
-        {
-          filteredMonster.map((monster) => {
-            return ( 
-            <div key={monster.id}>
-              <h1>{monster.name} </h1> 
-            </div>
-            )
-          })
-        }
+        <SearchBox onChangeHandler = {onSearchChange} placeholder='search-monsters' className='search-box'/>
+        <CardList monsters={filteredMonster}/>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        const { monsters } = this.props;
+        return (
+            <div>
+                {monsters.map(monsters => (
+                    <h1 key={monsters.id}>{monsters.name}</h1>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default CardList;
