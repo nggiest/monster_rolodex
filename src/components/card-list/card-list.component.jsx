a2e07a0533a2175a8f610798f811e6399707b008
agrgiova8892 <giovanni.anggiesta@ai.astra.co.id>
2023-02-25 00:33:37 +0700

    progress

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
 
+import './card-list.styles.css';
+
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
         return (
             <div className="card-list">
-                {monsters.map(monsters => (
-                    <h1 key={monsters.id}>{monsters.name}</h1>
-                ))}
+                {monsters.map((monsters) => {
+                    const {name, email, id} = monsters;
+                    return (
+                    <div className='card-container' key = {id}> 
+                        <img 
+                            alt={`monsters ${name}`}
+                            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                        />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                    )})}
             </div>
         )
     }
